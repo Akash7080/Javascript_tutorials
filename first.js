82e09a6fc55cabc7ab8abce0dbd68378bd5cb14a
Akash Singh <singhakash1071990@gmail.com>
2023-08-05 12:35:28 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,97 @@
+// switch case to vowel and consnant
+
+// even odd using switch
+//
+//
+//
+//LOOPS
+
+// 1.while
+// 2. do while
+// 3.for
+// 4.for in loop
+// 5.for of loop
+// 6. foreach lopp
+
+// 1.while (condition) {
+
+//     statement
+
+//     update
+// }
+
+// var j=100;
+// while(j>=90)
+// {
+//     console.log(j);
+//     j--;
+// }
+
+
+// var j=1;
+// sum=0;
+// while(j<=10)
+// {
+//     console.log(j);
+   
+//     sum=sum+j;
+//     j++;
+//     console.log(sum);
+// }
+
+
+// var j=1;
+// mul=2;
+// while(j<=10)
+// {
+    
+   
+//     mul=mul*j;
+//     console.log(mul);
+//     j++;
+    
+// }
+
+
+// var i=1;
+// while(i<=10)
+// {
+//     i++;
+//     if(i==6 || i==8)
+//     {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
+// 2.Do while
+
+// do{
+//     statment
+// }while (condition)
+
+
+// var i=7;
+
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=15)
+
+// 3.for loop
+
+// for(initialization:condition;update)
+// {
+//     statment;
+// }
+ 
+// for (var i=10;i>=10;i--)
+// {
+//     console.log(i);
+// }
+
+
+// Difference between let var and const
+
+// var is outside the block but let can not be acess outside the block.const can not be the run in the loop
